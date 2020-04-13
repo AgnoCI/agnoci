85d66a3aabcd90e328c3defbe5895475c079916e
Adam Brady <adam@boxxen.org>
2020-04-14 00:17:54 +1000

    Flesh out e2e demo slightly more

--- examples/e2e/pipeline.ts
+++ examples/e2e/pipeline.ts
@@ -1,15 +1,42 @@
-import { Pipeline, Target, single, command, manual, stopUnlessBranch } from '../packages/@agnoci/core'
+import { Pipeline, Target, single, command, manual, stopUnlessBranch, Node, env } from '../../packages/@agnoci/core'
+import { npm } from '../../packages/@agnoci/node'
+import * as docker from '../../packages/@agnoci/docker'
+
+function getImage () {
+  return {
+    image: 'e2e-example',
+    tag: `${env.branch()()}-${env.commit()()}`
+  }
+}
+
+function test (): Node {
+  return single([
+    npm.install({ noOptional: true }),
+    npm.test()
+  ])
+}
+
+function build (): Node {
+  const image = getImage()
+
+  return single([
+    docker.build(image),
+    docker.push(image)
+  ])
+}
 
 function deploy (env: string) {
+  const image = getImage()
+
   return single([
     command('kubectl config set-cluster k8s --server="${SERVER}"'),
     command('kubectl config set clusters.k8s.certificate-authority-data ${CERTIFICATE_AUTHORITY_DATA}'),
     command('kubectl config set-credentials gitlab --token="${USER_TOKEN}"'),
     command('kubectl config set-context default --cluster=k8s --user=gitlab'),
     command('kubectl config use-context default'),
-    command('sed -i "s/<TAG>/${TAG}/g" deployment.yaml'),
+    command(`sed -i "s/<TAG>/${image.tag}/g" deployment.yaml`),
     command(`sed -i "s/<ENV>/${env}/g" deployment.yaml`),
-    command('echo "Deploying image - ${CONTAINER_IMAGE}"'),
+    command(`echo "Deploying image - ${image.image}:${image.tag}"`),
     command('kubectl apply -f account.yaml'),
     command('kubectl apply -f deployment.yaml'),
     command('kubectl apply -f service.yaml')
@@ -17,10 +44,11 @@ function deploy (env: string) {
 }
 
 const pipeline = new Pipeline({
-  target: Target.Bash
+  target: Target.Buildkite
 })
 
-pipeline.append(command('echo No tests yet'))
+pipeline.append(test())
+pipeline.append(build())
 pipeline.append(stopUnlessBranch('master'))
 pipeline.append(deploy('staging'))
 pipeline.append(manual())
